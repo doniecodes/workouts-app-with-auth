be663e70bbbb9ba7535bb9bfbdc63193e758f257
Donald <doniecode@gmail.com>
2025-12-28 14:43:22 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 // middleware
 app.use(express.json())
 app.use(cors({
-    target: "https://workouts-auth-frontend.onrender.com/",
+    target: "https://doniecode-workouts-app.netlify.app/",
     methods: [ "GET", "POST", "DELETE", "PUT", "PATCH" ]
 }))
 
